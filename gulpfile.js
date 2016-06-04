b6c5c6f9af38642f01831c639bce6e62415a22bd
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-06-04 21:42:03 +0100

    Make build configurable
    - Remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,32 @@
-'use strict';
-
 const gulp = require('gulp');
-
 const addSrc = require('gulp-add-src');
 const concat = require('gulp-concat');
 const insert = require('gulp-insert');
 const remoteSrc = require('gulp-remote-src');
 const replace = require('gulp-replace');
-const uglify = require('gulp-uglify');
-
-const BENCHMARKJS_VERSION = '2.1.0';
-const requestOptions = {
-	'gzip': true,
-	'strictSSL': true
-};
+const config =  require('./config.js');
 
 gulp.task('js', function() {
 
 	// Fetch Platform.js, Benchmark.js, and its jsPerf UI dependencies.
 	remoteSrc([
 		'bestiejs/platform.js/1.3.1/platform.js',
-		`bestiejs/benchmark.js/${ BENCHMARKJS_VERSION }/benchmark.js`,
-		`bestiejs/benchmark.js/${ BENCHMARKJS_VERSION }/example/jsperf/ui.js`,
-		`bestiejs/benchmark.js/${ BENCHMARKJS_VERSION }/plugin/ui.browserscope.js`
+		`bestiejs/benchmark.js/${config.benchmarkjs.version}/benchmark.js`,
+		`bestiejs/benchmark.js/${config.benchmarkjs.version}/example/jsperf/ui.js`,
+		`bestiejs/benchmark.js/${config.benchmarkjs.version}/plugin/ui.browserscope.js`
 	], {
 			'base': 'https://raw.githubusercontent.com/',
-			'requestOptions': requestOptions
+			'requestOptions': config.requestOptions
 		}
 	)
 
 	// Use whatever version of lodash Benchmark.js is using.
 	.pipe(addSrc.prepend(require.resolve('lodash')))
 
-	.pipe(concat('benchmark.js'))
+	.pipe(concat(config.output.file))
 
 	// Set the Google Analytics ID.
-	.pipe(replace('gaId = \'\'', 'gaId = \'UA-6065217-40\''))
+	.pipe(replace('gaId = \'\'', `gaId = \'${config.ga}\'`))
 
 	// jsPerf is browser-only. Ensure we’re detected as a browser environment,
 	// even if this is an AMD test, for example.
@@ -74,8 +65,8 @@ gulp.task('assets', function() {
 		'index.html',
 		'main.css'
 	], {
-			'base': `https://raw.githubusercontent.com/bestiejs/benchmark.js/${ BENCHMARKJS_VERSION }/example/jsperf/`,
-			'requestOptions': requestOptions
+			'base': `https://raw.githubusercontent.com/bestiejs/benchmark.js/${config.benchmarkjs.version}/example/jsperf/`,
+			'requestOptions': config.requestOptions
 		}
 	)
 	.pipe(replace('<script src="../../node_modules/lodash/index.js"></script>', ''))
